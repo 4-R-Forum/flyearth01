d5b9d0d8a5617d9e8a92e9ad4b8bdaee7d736ad2
4-R-Forum <jhodge@4-R-Forum.org>
2023-08-30 12:25:34 -0400

    Se Actions Keys

--- FlyEarth-02.ps1
+++ FlyEarth-02.ps1
@@ -1,10 +1,20 @@
 ﻿Add-Type -Path "C:\Portable\Selenium\WebDriver.dll"
 $driver = New-Object OpenQA.Selenium.Chrome.ChromeDriver
 $driver.Navigate().GoToURL("https://earth.google.com/") | Out-Null
-$By = [OpenQA.Selenium.By]
+#$By = [OpenQA.Selenium.By]
 $driver.Manage().Timeouts().ImplicitWait = [System.TimeSpan]::FromSeconds(4)
 Read-Host "Navigate to starting point"
-$down = $driver.FindElement($By::Id("zoom-in"))
+#$earth_app = $driver.FindElements($By::XPath("/html/body/earth-app"))
+#$down = $earth_app.FindElement($By::Id("zoom-in"))
+#$down
+$action = New-Object OpenQA.Selenium.Interactions.Actions($driver)
+#$action.KeyDown([OpenQA.Selenium.Keys]::Shift)
+$Keys = New-Object OpenQA.Selenium.Keys($action)
+for ($i = 0; $i -lt 10; $i++) {
+  $action.KeyDown($Keys.ARROW_UP)
+  #$action.SendKeys("a")
+  $action.Perform()
+}
 
 
 $driver.Close()
